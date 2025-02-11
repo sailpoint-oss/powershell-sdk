ae36f33f88cf09a54425333277abc422bbd2d3a4
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2025-02-11 16:28:17 -0500

    fixes for after running prescript

--- PSSailpoint/v3/src/PSSailpoint.V3/Api/SearchApi.ps1
+++ PSSailpoint/v3/src/PSSailpoint.V3/Api/SearchApi.ps1
@@ -248,7 +248,7 @@ A switch when turned on will return a hash table of Response, StatusCode and Hea
 
 .OUTPUTS
 
-SearchDocument
+SystemCollectionsHashtable
 #>
 function Search-Get {
     [CmdletBinding()]
@@ -301,11 +301,8 @@ function Search-Get {
                                 -QueryParameters $LocalVarQueryParameters `
                                 -FormParameters $LocalVarFormParameters `
                                 -CookieParameters $LocalVarCookieParameters `
-                                -ReturnType "SearchDocument" `
+                                -ReturnType "SystemCollectionsHashtable" `
                                 -IsBodyNullable $false
-
-        # process oneOf response
-        $LocalVarResult["Response"] = ConvertFrom-JsonToSearchDocument (ConvertTo-Json $LocalVarResult["Response"] -Depth 100)
 
         if ($WithHttpInfo.IsPresent) {
             return $LocalVarResult
@@ -342,7 +339,7 @@ A switch when turned on will return a hash table of Response, StatusCode and Hea
 
 .OUTPUTS
 
-SearchDocument[]
+SystemCollectionsHashtable[]
 #>
 function Search-Post {
     [CmdletBinding()]
@@ -424,7 +421,7 @@ function Search-Post {
                                 -QueryParameters $LocalVarQueryParameters `
                                 -FormParameters $LocalVarFormParameters `
                                 -CookieParameters $LocalVarCookieParameters `
-                                -ReturnType "SearchDocument[]" `
+                                -ReturnType "SystemCollectionsHashtable[]" `
                                 -IsBodyNullable $false
 
         if ($WithHttpInfo.IsPresent) {

d8a143e6b2a584ffaadf8fbc6b3b89d36fa5555c
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2025-06-02 14:09:38 +0000

    Bump version to v1.6.2

--- PSSailpoint/v3/src/PSSailpoint.V3/Private/ApiClient.ps1
+++ PSSailpoint/v3/src/PSSailpoint.V3/Private/ApiClient.ps1
@@ -77,8 +77,8 @@ function Invoke-ApiClient {
     }
 
     # Add Custom Header
-    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.6.1"
-    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.6.1/ps"
+    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.6.2"
+    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.6.2/ps"
 
 
     $HasFormData = $False

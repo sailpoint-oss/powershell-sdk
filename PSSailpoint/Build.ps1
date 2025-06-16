5c5e8daa039a83d670edf5f02c93d44b2a724a48
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2025-06-16 13:34:24 +0000

    Bump version to v1.6.3

--- PSSailpoint/Build.ps1
+++ PSSailpoint/Build.ps1
@@ -55,7 +55,7 @@ $Manifest = @{
     CompanyName       = 'SailPoint Technologies'
     Description       = 'PSSailpoint - the PowerShell module for IdentityNow'
 
-    ModuleVersion     = '1.6.2'
+    ModuleVersion     = '1.6.3'
 
     RootModule        = 'PSSailpoint.psm1'
     Guid              = '7A197170-97E8-4DCD-A171-271D4AEC2F36' # Has to be static, otherwise each new build will be considered different module
@@ -65,10 +65,10 @@ $Manifest = @{
     FunctionsToExport = $FunctionPath | Get-ChildItem -Filter *.ps1 | Get-FunctionsToExport
 
     RequiredModules = @(
-        @{ModuleName = 'PSSailpoint.Beta'; RequiredVersion = '1.6.2'; }, 
-        @{ModuleName = 'PSSailpoint.V3'; RequiredVersion = '1.6.2'; },
-        @{ModuleName = 'PSSailpoint.V2024'; RequiredVersion = '1.6.2'; },
-        @{ModuleName = 'PSSailpoint.V2025'; RequiredVersion = '1.6.2'; }
+        @{ModuleName = 'PSSailpoint.Beta'; RequiredVersion = '1.6.3'; }, 
+        @{ModuleName = 'PSSailpoint.V3'; RequiredVersion = '1.6.3'; },
+        @{ModuleName = 'PSSailpoint.V2024'; RequiredVersion = '1.6.3'; },
+        @{ModuleName = 'PSSailpoint.V2025'; RequiredVersion = '1.6.3'; }
     )
 
     VariablesToExport = @()
--- PSSailpoint/v3/src/PSSailpoint.V3/Private/ApiClient.ps1
+++ PSSailpoint/v3/src/PSSailpoint.V3/Private/ApiClient.ps1
@@ -77,8 +77,8 @@ function Invoke-ApiClient {
     }
 
     # Add Custom Header
-    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.6.2"
-    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.6.2/ps"
+    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.6.3"
+    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.6.3/ps"
 
 
     $HasFormData = $False

246ba1651c60720c731a04d1b68c8864b1e652e0
developer-relations-sp <123671052+developer-relations-sp@users.noreply.github.com>
2026-02-18 22:06:09 +0000

    Bump version to v1.7.5

--- PSSailpoint/v2024/src/PSSailpoint.V2024/Private/V2024ApiClient.ps1
+++ PSSailpoint/v2024/src/PSSailpoint.V2024/Private/V2024ApiClient.ps1
@@ -77,8 +77,8 @@ function Invoke-V2024ApiClient {
     }
 
     # Add Custom Header
-    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.7.4"
-    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.7.4/ps"
+    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.7.5"
+    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.7.5/ps"
 
 
     $HasFormData = $False
--- PSSailpoint/v2025/src/PSSailpoint.V2025/PSSailpoint.V2025.psd1
+++ PSSailpoint/v2025/src/PSSailpoint.V2025/PSSailpoint.V2025.psd1
@@ -12,7 +12,7 @@
 RootModule = 'PSSailpoint.V2025.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.7.4'
+ModuleVersion = '1.7.5'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()

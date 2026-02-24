7efa456923697d32685a4505e2afd502194bcdfa
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-02-24 08:13:42 +0000

    Bump version to v1.7.13

--- PSSailpoint/beta/src/PSSailpoint.Beta/Private/BetaApiClient.ps1
+++ PSSailpoint/beta/src/PSSailpoint.Beta/Private/BetaApiClient.ps1
@@ -77,8 +77,8 @@ function Invoke-BetaApiClient {
     }
 
     # Add Custom Header
-    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.7.12"
-    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.7.12/ps"
+    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.7.13"
+    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.7.13/ps"
 
 
     $HasFormData = $False
--- PSSailpoint/v2026/src/PSSailpoint.V2026/Private/V2026ApiClient.ps1
+++ PSSailpoint/v2026/src/PSSailpoint.V2026/Private/V2026ApiClient.ps1
@@ -77,8 +77,8 @@ function Invoke-V2026ApiClient {
     }
 
     # Add Custom Header
-    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.7.12"
-    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.7.12/ps"
+    $HeaderParameters['X-SailPoint-SDK'] = "Powershell-1.7.13"
+    $HeaderParameters['User-Agent'] = "OpenAPI-Generator/1.7.13/ps"
 
 
     $HasFormData = $False

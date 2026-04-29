9a558bafc2343988dc3c6e02c82ab317aa85257d
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-04-29 08:45:33 +0000

    Bump version to v1.7.60

--- PSSailpoint/nerm/src/PSSailpoint.NERM/Private/NERMApiClient.ps1
+++ PSSailpoint/nerm/src/PSSailpoint.NERM/Private/NERMApiClient.ps1
@@ -77,7 +77,7 @@ function Invoke-NERMApiClient {
     }
 
     # Build User-Agent header
-    $UserAgent = "SailPoint-SDK-PowerShell/1.7.59"
+    $UserAgent = "SailPoint-SDK-PowerShell/1.7.60"
     if ($Configuration["ConsumerIdentifier"] -and $Configuration["ConsumerVersion"]) {
         $UserAgent = "$UserAgent ($($Configuration["ConsumerIdentifier"])/$($Configuration["ConsumerVersion"]))"
     }
--- PSSailpoint/nermV2025/src/PSSailpoint.NERMV2025/Private/NERMV2025ApiClient.ps1
+++ PSSailpoint/nermV2025/src/PSSailpoint.NERMV2025/Private/NERMV2025ApiClient.ps1
@@ -77,7 +77,7 @@ function Invoke-NERMV2025ApiClient {
     }
 
     # Build User-Agent header
-    $UserAgent = "SailPoint-SDK-PowerShell/1.7.59"
+    $UserAgent = "SailPoint-SDK-PowerShell/1.7.60"
     if ($Configuration["ConsumerIdentifier"] -and $Configuration["ConsumerVersion"]) {
         $UserAgent = "$UserAgent ($($Configuration["ConsumerIdentifier"])/$($Configuration["ConsumerVersion"]))"
     }

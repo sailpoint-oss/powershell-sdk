f5e07ce09605d94990aefd0a1e9debc5b377b1fb
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-05-01 08:38:33 +0000

    Bump version to v1.7.62

--- PSSailpoint/v3/src/PSSailpoint.V3/Private/ApiClient.ps1
+++ PSSailpoint/v3/src/PSSailpoint.V3/Private/ApiClient.ps1
@@ -77,7 +77,7 @@ function Invoke-ApiClient {
     }
 
     # Build User-Agent header
-    $UserAgent = "SailPoint-SDK-PowerShell/1.7.61"
+    $UserAgent = "SailPoint-SDK-PowerShell/1.7.62"
     if ($Configuration["ConsumerIdentifier"] -and $Configuration["ConsumerVersion"]) {
         $UserAgent = "$UserAgent ($($Configuration["ConsumerIdentifier"])/$($Configuration["ConsumerVersion"]))"
     }
